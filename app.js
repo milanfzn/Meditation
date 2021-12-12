dec9d9e3818e4292202bd24c2aa5119263cbd2d6
Milan Tomasevic <milan.fz.tomas@gmail.com>
2021-12-12 15:22:01 +0100

    Added  all files and started working on app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline cirlce");
+  const video = document.querySelector(".vid-container video");
+
+  //Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+  //Time Display
+  const timeDisplay = document.querySelector(".time-display");
+};
